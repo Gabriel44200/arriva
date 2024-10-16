1f79aceeae9b8e4a1457560b2dd86b1245e09f81
Gabriel Montes <64473570+Gabriel44200@users.noreply.github.com>
2024-10-16 16:50:28 +0200

    Update known_servers.py

--- known_servers.py
+++ known_servers.py
@@ -1,19 +1,4 @@
-XG_EXTERNOS = "https://tpgal-ws-externos.xunta.gal/tpgal_ws/rest"
+ARRIVA = "https://arriva.gal/plataforma/api/"
 """
-Public API for individuals
-"""
-
-XG_APP = "https://tpgal-ws.xunta.gal/tpgal_ws/rest"
-"""
-The API the Android app uses
-"""
-
-XG_XNQR_APP = "https://xentenovaqr.xunta.gal/api"
-"""
-The API the Android app uses for the Xente Nova QR functionality
-"""
-
-XG_REALTIME_APP = "https://sae-its.xunta.gal:8019"
-"""
-The SOAP API the Android app uses
+API usada por web y apps
 """
